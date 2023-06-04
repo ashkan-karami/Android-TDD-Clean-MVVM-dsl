09f92bdafee6abf52aa51f48050f5c53fdef872c
a.karami <ashkan.k.9877@gmail.com>
2023-06-04 19:10:58 +0330

    added two unit tests for phone nu,ber validation

--- common/core/src/main/java/com/ashkan/userprofile/common/core/validation/UserInformation.kt
+++ common/core/src/main/java/com/ashkan/userprofile/common/core/validation/UserInformation.kt
@@ -1,5 +1,11 @@
 package com.ashkan.userprofile.common.core.validation
 
+/**
+ *  Checks if entered passport number is correct.
+ */
 fun String.isAValidPassport(): Boolean = this.matches("^(?!^0+\$)[a-zA-Z0-9]{3,20}\$".toRegex())
 
+/**
+ *  Checks if entered phone number is correct.
+ */
 fun String.isAValidPhoneNumber(): Boolean = this.matches("^\\+[0-9]{10,13}$".toRegex())
--- common/core/src/test/java/com/ashkan/userprofile/common/core/validationTest/UserInformationUnitTest.kt
+++ common/core/src/test/java/com/ashkan/userprofile/common/core/validationTest/UserInformationUnitTest.kt
@@ -1,20 +1,46 @@
 package com.ashkan.userprofile.common.core.validationTest
 
 import com.ashkan.userprofile.common.core.validation.isAValidPassport
+import com.ashkan.userprofile.common.core.validation.isAValidPhoneNumber
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class UserInformationUnitTest {
 
+    /**
+     *  Test passport validation with correct data.
+     */
     @Test
     fun `passport is correct`(){
         val pass = "P51659797"
         assertTrue(pass.isAValidPassport())
     }
 
+    /**
+     *  Test passport validation with incorrect data.
+     */
     @Test
     fun `passport is not correct`(){
         val pass = "A12345@"
-        assertTrue(pass.isAValidPassport())
+        assertFalse(pass.isAValidPassport())
+    }
+
+    /**
+     *  Test phone number validation with correct data.
+     */
+    @Test
+    fun `phone-number is correct`(){
+        val phone = "+1456646545"
+        assertTrue(phone.isAValidPhoneNumber())
+    }
+
+    /**
+     *  Test phone number validation with incorrect data.
+     */
+    @Test
+    fun `phone-number is not correct`(){
+        val phone = "915664345"
+        assertFalse(phone.isAValidPhoneNumber())
     }
 }
