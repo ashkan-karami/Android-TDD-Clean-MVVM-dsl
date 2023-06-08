13fdc62230440566607063b80f59ab525bc56b46
a.karami <ashkan.k.9877@gmail.com>
2023-06-08 21:19:44 +0330

    a small navigate added

--- app/src/main/java/com/ashkan/userprofile/MainActivity.kt
+++ app/src/main/java/com/ashkan/userprofile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ashkan.userprofile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.ashkan.userprofile.common.navigation.Navigator
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.ref.WeakReference
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Navigator.navControllerReference = WeakReference(findNavController(R.id.navHost))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        Navigator.navControllerReference = WeakReference(navHostFragment.navController)
     }
 }
