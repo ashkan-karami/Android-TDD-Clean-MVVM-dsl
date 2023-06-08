b6508b9a4b2818ffe7c95778d140de3f552810a7
a.karami <ashkan.k.9877@gmail.com>
2023-06-08 16:37:24 +0330

    feature module added, a new feature named profile added contains of two modules, data and domain

---
+++ features/profile/domain/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+
+    api(project(":common:base_domain"))
+    testImplementation(TestLibs.junit)
+}
