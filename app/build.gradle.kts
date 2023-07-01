3a865fd7ca1a5e89575859d8b18126ad19e8a5a8
ashkan.karami <ashkan.k.9877@gmail.com>
2023-07-01 15:32:53 +0330

    gradle upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
     }
     dynamicFeatures += setOf(Features.profileFeature, Features.loginFeature)
 }
